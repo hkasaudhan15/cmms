3496c83c41f716ad5ef446a514472d7dc51d3e1d
solace06 <khushi.sharma061101@gmail.com>
2025-09-17 11:44:29 +0530

    Added function call to initialize database

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	//initializing db
+	db, _, err := NewDB(ctx)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Printf("Using database: %v", db.Name())
+
+	//setting up router and routes
+	router := mux.NewRouter()
+
+
+	//Intialising server
+	fmt.Println("server running on port 5500")
+	if err := http.ListenAndServe(":5500", router); err != nil {
+		log.Fatal(err)
+	}
+}
