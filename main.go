d8da94d5b51ed6b0b53dde5f2efb924762c8cea1
Rohit Bajiya <99985753+erbajiya@users.noreply.github.com>
2025-09-18 16:57:21 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,53 +3,39 @@ package main
 import (
 	"context"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
-	"go.mongodb.org/mongo-driver/mongo"
-)
-
-// Declare these global variables here so they're accessible across files
-var (
-	db        *mongo.Database
-	client    *mongo.Client
-	templates *template.Template
 )
 
 func main() {
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	// Initialize database
-	db, client, err := NewDB(ctx)
+	//initializing db
+	db, _, err := NewDB(ctx)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	defer client.Disconnect(ctx)
 
-	// Set global variables
-	setDB(db, client)
+	fs := http.FileServer(http.Dir("style"))
+	http.Handle("/style/", http.StripPrefix("/style/", fs))
 
-	// Initialize templates - include nested template files (e.g. templates/maintenence/*.html)
-	templates = template.Must(template.ParseGlob("templates/**/*.html"))
+	http.HandleFunc("/service", serviceListHandler)
+	http.HandleFunc("/service/create", serviceCreateHandler)
+	http.HandleFunc("/service/edit", serviceEditHandler)
+	http.HandleFunc("/service/delete", serviceDeleteHandler)
+
+	http.HandleFunc("/consumable", consumableListHandler)
+	http.HandleFunc("/consumable/create", consumableCreateHandler)
+	http.HandleFunc("/consumable/edit", consumableEditHandler)
+	http.HandleFunc("/consumable/delete", consumableDeleteHandler)
+
+	
 
 	fmt.Printf("Using database: %v", db.Name())
 
-	//setting up router and routes
-	router := mux.NewRouter()
-
 	//Intialising server
-	fmt.Println("server running on port 5500")
-	if err := http.ListenAndServe(":5500", router); err != nil {
-		log.Fatal(err)
-	}
-}
-
-// SetDB function to set the global database variable
-func setDB(database *mongo.Database, c *mongo.Client) {
-	db = database
-	client = c
+	http.ListenAndServe(":8080", nil)
 }
