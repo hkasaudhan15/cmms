baecab66b09f12b96877b4fca9009900ad31ddab
hkasaudhan15 <147414548+hkasaudhan15@users.noreply.github.com>
2025-09-18 16:19:15 +0530

    My local changes before pull

--- main.go
+++ main.go
@@ -3,33 +3,65 @@ package main
 import (
 	"context"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// Declare these global variables here so they're accessible across files
+var (
+	db        *mongo.Database
+	client    *mongo.Client
+	templates *template.Template
 )
 
 func main() {
-
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	//initializing db
-	db, _, err := NewDB(ctx)
+	// Initialize database
+	db, client, err := NewDB(ctx)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
+	defer client.Disconnect(ctx)
 
-	fmt.Printf("Using database: %v", db.Name())
+	// Set global variables
+	setDB(db, client)
 
-	//setting up router and routes
+	// Initialize templates - include nested template files (e.g. templates/maintenence/*.html)
+	templates = template.Must(template.ParseGlob("templates/**/*.html"))
+
+	fmt.Printf("Using database: %v\n", db.Name())
+
+	// Setting up router and routes
 	router := mux.NewRouter()
 
+	// Serve static files
+	router.PathPrefix("/style/").Handler(http.StripPrefix("/style/", http.FileServer(http.Dir("style/"))))
 
-	//Intialising server
-	fmt.Println("server running on port 5500")
-	if err := http.ListenAndServe(":5500", router); err != nil {
+	// Maintenance Routes
+	router.HandleFunc("/maintenances", listMaintenance)
+	router.HandleFunc("/maintenances/create", createMaintenance)
+	router.HandleFunc("/maintenances/edit", editMaintenance)
+	router.HandleFunc("/maintenances/view", viewMaintenance)
+	router.HandleFunc("/maintenances/delete", deleteMaintenance)
+	router.HandleFunc("/shedules/add", addShedule)
+	router.HandleFunc("/shedules/delete", deleteShedule)
+
+	// Initializing server
+	fmt.Println("server running on port 8080")
+	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// SetDB function to set the global database variable
+func setDB(database *mongo.Database, c *mongo.Client) {
+	db = database
+	client = c
 }
--- models.go
+++ models.go
@@ -1,1 +1,20 @@
 package main
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Shedule struct {
+	ID          primitive.ObjectID   `bson:"_id"`
+	Lable       string               `bson:"label"`
+	SheduleType string               `bson:"shedule_type"`
+	Days        int                  `bson:"days"`
+	Services    []primitive.ObjectID `bson:"services"`
+	Consumables []primitive.ObjectID `bson:"consumables"`
+	Notes       string               `bson:"notes"`
+}
+
+type MainteneceShedule struct {
+	ID       primitive.ObjectID `bson:"_id"`
+	Lable    string             `bson:"label"`
+	AssetID  primitive.ObjectID `bson:"asset_id"`
+	Shedules []Shedule          `bson:"shedules"`
+}
